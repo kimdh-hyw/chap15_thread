fa1f428db15420c00e36f47602059023229e443d
kimdh-hyw <71587564+kimdh-hyw@users.noreply.github.com>
2021-06-25 09:30:06 +0900

    comments correction

--- src/sec03_threadproperties/EX05_ThreadProperties_3_3/ThreadProperties_3_3.java
+++ src/sec03_threadproperties/EX05_ThreadProperties_3_3/ThreadProperties_3_3.java
@@ -16,13 +16,13 @@ public class ThreadProperties_3_3 {
 		
 		//#1. 일반쓰레드
 		Thread thread1 = new MyThread();
-		thread1.setDaemon(false);//디폴트
+		thread1.setDaemon(false);// 일반쓰레드로 설정
 		thread1.setName("thread1");
 		thread1.start();
 		
 		//#2. 데몬쓰레드
 		Thread thread2 = new MyThread();
-		thread2.setDaemon(true);//디폴트
+		thread2.setDaemon(true);// 데몬쓰레드로 설정
 		thread2.setName("thread2");
 		thread2.start();
 		
